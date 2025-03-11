7bc16f8325b1a96fe3d90c853e96a305af6a35a5
Hassan-Madridian <hassanahmed1930@gmail.com>
2025-03-11 16:41:53 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Botpress Webchat configuration
+window.botpressWebChat.init({
+    host: 'https://cdn.botpress.cloud/webchat/v2.2',
+    botId: 'your-bot-id-here',
+    messagingUrl: 'https://files.bpcontent.cloud/2025/03/06/21/20250306213835-YTHP4YIH.js',
+    container: '#chatbot',
+    // Optional customization
+    chatTitle: "AI Chatbot",
+    chatSubtitle: "Ask me anything!",
+});
